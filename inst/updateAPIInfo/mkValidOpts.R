55389b560a8df4e6517e5ee31774bb79e2cad055
Duncan Temple Lang <duncan@r-project.org>
2017-01-26 15:41:30 -0800

    moved meta-code out of R for R CMD check issues

--- inst/updateAPIInfo/mkValidOpts.R
+++ inst/updateAPIInfo/mkValidOpts.R
@@ -4,12 +4,12 @@ updateValidOptions =
     # to generate the ValidOptions variable in the package
     # that is used by the valid_opts() function.
     # It reads the names of the valid parameters from the API documentation page
-    # so that they are "up-to-date".  Run this function in the R
+    # so that they are "up-to-date".  Run this function in this
     # directory of the package with
     #  updateValidOtions()
-    # This regenerates the file ValidOpts.R
+    # This regenerates the file ValidOpts.R in the R/ directory.
     #
-function(filename = "ValidOpts.R")    
+function(filename = "../../R/ValidOpts.R")    
 {
     tbls <- readHTMLTable("http://et.water.ca.gov/Rest/Index", stringsAsFactors = FALSE)[c("Daily Data Items", "Hourly Data Items")  ]
     items = lapply(tbls, `[[`, "Data Item")
