1b0fa73f96ad77eab0d85c10921bdd620a053a47
Duncan Temple Lang <duncan@r-project.org>
2017-01-27 07:19:57 -0800

    fix to not use o but items, and also dump all tables and not just the data items column from each

--- inst/updateAPIInfo/mkValidOpts.R
+++ inst/updateAPIInfo/mkValidOpts.R
@@ -14,11 +14,10 @@ updateValidOptions =
 function(filename = "../../R/ValidOpts.R")    
 {
     tbls <- readHTMLTable("http://et.water.ca.gov/Rest/Index", stringsAsFactors = FALSE)#[c("Daily Data Items", "Hourly Data Items")  ]
-    items = lapply(tbls, `[[`, "Data Item")
 
     if(length(filename) && !is.na(filename) && filename != "") {
-        txt = capture.output(dput(o))
-        cat("ValidOptions <- ", txt, sep = "\n", file = filename)
+       txt = capture.output(dput(items))
+       cat("ValidOptions <- ", txt, sep = "\n", file = filename)
     }
     items
 }
