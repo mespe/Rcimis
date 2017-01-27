0bb5e7b13f63e0f2d98c7cf537c4236363254f80
Matt Espe <mespe@ucdavis.edu>
2017-01-27 15:32:41 -0800

    Added docs for getDists

--- R/getNearestStation.R
+++ R/getNearestStation.R
@@ -1,3 +1,19 @@
+##' Get geographic distance to nearest CIMIS stations
+##'
+##' This function gets the nearest n stations from a geographic point.
+##' 
+##' @title getDists
+##' @param lat The latitude, in decimal degrees
+##' @param lon longitude, in decimal degrees
+##' @param n number of nearest stations to return
+##' @param FUN the function used to calculate the distance. Passed to the \code{distm} function in the \package{geospheres} package
+##' @return a subset of \code{StnInfo} ordered by distance to the specificed point, with the distance in Km added
+##' @author Matt Espe
+##'
+##' @examples
+##' #Get the 5 closest stations
+##' getDists(37,8, -121.22, n = 5)
+##' 
 getDists <- function(lat, lon, n = 1, FUN = distHaversine){
     ## Only compare stations which are close (within a degree)
     close <- (diff(StnInfo$DdLatitude - lat) <= 1) & (diff(StnInfo$DdLongitude - lon) <= 1)
